288a18e96b67f515318d9f581e8dd73b63b1ee15
sarahkadi <saarah.kadi@gmail.com>
2020-10-31 22:23:22 +0100

    <Implementation> : fin de l'implementation des fonctions du fichier "main.py" et modification légère du fichier "products.txt"  On branch main

--- main.py
+++ main.py
@@ -1,32 +1,78 @@
+# -*- coding:Utf-8 -*-
 import re
+import sqlite3
+conn = sqlite3.connect('my.db')
+cur = conn.cursor()
 
-#Ouverture du fichier en lecture
-file = open('produits.txt',"r")
-lines = file.readlines()
+#*********************** OUVERTURE DU FICHIER PRODUITS.TXT ET LECTURE DU CONTENU ************************
 
-#initialisation d'une liste vide
-liste = []
-for line in lines:
-    #Enlever l'espace et le \n de ma cdc
-    line = re.split(' |\n',line) 
+#Initialisation d'une liste vide de produits
+listOfProducts = []
+#--Ouverture du fichier en lecture
+def openingFile():
+    file = open('products.txt',"r")
+    lines = file.readlines()
+    listOfProductsTmp = []
+    for line in lines:
+    #Enlever l'espace et le \n de ma chaine de caractères
+        line = re.split(' |\n',line) 
     #Supprimer les '' de la liste
-    if '' in line:
-        line.remove('')
+        if '' in line:
+            line.remove('')
     #Concatenation des deux chaines
-    liste += line
-    #print(line)
-print(liste)   
-
-#Initialisation des produits
-liste2 = ['Stylo','Crayon','Trousse']
-i = 0
-while i < len(liste):
-    if liste[i] in liste2:
-        print(liste[i]+ " existe dans la liste des produits")
-    else:
-        print(liste[i]+ " n'existe pas dans la liste des produits")
-    i += 2
+        listOfProductsTmp += line
+    print(listOfProductsTmp)
+    file.close() 
+    return listOfProductsTmp
+   
     
-       
+#************************* GESTION DE LA BASE DE DONNEES *************************************************
+#Les produits achetés dans le fichier "produits.txt" devront être écris sous la forme: PRODUIT QUANTITE(int)
+
+#--Affichage de la base de données
+def displayBDD():
+    querry = "SELECT * FROM magasin"
+    magasinTable = cur.execute(querry).fetchall()
+    print(magasinTable)
+#--Modification de la base de données (décrementation des produits achetés)
+def updateBDD():
+    listOfProducts = openingFile()  
+    with conn:
+        querry = "SELECT * FROM magasin"
+        magasinTable = cur.execute(querry).fetchall()
+    #Parcours de la liste récuperée du fichier
+        for i in range (len(listOfProducts)):
+    #Parcous des lignes de la table "magasin"
+            for line in magasinTable :
+                if listOfProducts[i] in line:
+    #Initialisation de la nouvelle variable représentant la nouvelle quantité du produit après achat
+                    newqty = line[2] - int(listOfProducts[i+1])
+                    cur.execute("""UPDATE magasin SET quantite = :quantity
+                    WHERE produit = :product""", {'quantity':newqty, 'product':line[1]})
+
+#--Remplissage du stock pour chaque produit
+def fillStock():
+    with conn:
+        querry = "SELECT * FROM magasin"
+        magasinTable = cur.execute(querry).fetchall()
+        for line in magasinTable :
+            newqty = 100
+            cur.execute("""UPDATE magasin SET quantite = :quantity
+            WHERE produit = :product""", {'quantity':newqty, 'product':line[1]})
+
+#--Envoie d'une notification si le stock d'un produit est inférieur à 20
+def notifyEmptyStock():
+    with conn:
+        querry = "SELECT * FROM magasin"
+        magasinTable = cur.execute(querry).fetchall()
+        for line in magasinTable :
+            if line[2] < 30:
+                print("Attention, le produit " + line[1] + " est bientot epuisé")
 
 
+#************************* APPELS DES FONCTIONS **********************************************************
+
+#Appel de la fonction de mise à jour de la BDD
+updateBDD()
+#Affichage de la nouvelle table 
+displayBDD()
